9075d6047841e480ac30ec6eb541dafd951fe18d
William Kulha <kulha.william@gmail.com>
2018-10-09 12:09:17 -0400

    Add spec so that program user string length to determine the size of the encrypting square.

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,15 @@ let cleanUp = function(string) {
   let noSpaceNoPunctuation = string.replace(/[^\w\\s]|_/g, "")
   return noSpaceNoPunctuation.toLowerCase();
 }
+//function to calculate the size of the square for encryption.
+let squareSize = function(stringLength) {
+  let size = Math.sqrt(stringLength);
+  if (Number.isInteger(size)) {
+    return [size, size];
+  } else {
+    return [(parseInt(size) + 1), parseInt(size)]
+  }
+}
 
 
 
@@ -14,6 +23,10 @@ $(document).ready(function() {
   $('#user_form').submit(function(event) {
     event.preventDefault();
     let userString = $('#user_message').val();
-    console.log(cleanUp(userString));
+    if(userString) {
+      let cleanString = cleanUp(userString);
+      let size = squareSize(cleanString.length);
+      console.log(size);
+    }
   });
 });
