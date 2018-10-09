4bffc8328a409b24da598f8134fb43b7626fdd04
William Kulha <kulha.william@gmail.com>
2018-10-09 12:52:49 -0400

    Add spec so that the reformed array is broken into five letter chunks, joined, then added to a blank string.

--- js/scripts.js
+++ js/scripts.js
@@ -40,21 +40,44 @@ let makeGrid = function(string, size) {
 }
 //Function to read down the columns of the grid from left to right.
 let reform = function(twoDimensionalArray) {
+  //setting the number of rows to be equal to the length of the number of rows in the grid.
   let rows = twoDimensionalArray.length;
+  //setting columns to be equal to the number of columns in the grid.
   let columns = twoDimensionalArray[0].length;
-
+  //creating an array to push each letter to.
   let reformed = [];
-
+  //incrememnt rows to go from top to bottom rather than left to right.
   for(let row = 0; row < columns; row++) {
+    //incrememnt columns so that once we get to the end we move to the next row.
     for(let column = 0; column < rows; column ++) {
+      //this will return true as long as we are at a coordinate on the grid
       if(twoDimensionalArray[column][row]) {
         reformed.push(twoDimensionalArray[column][row]);
       }
     }
   }
+  //we return the initially blank array, once we have moved through every coordinate of the grid.
   return reformed;
 }
-
+//Function to join the reformed array at five character intervals.
+let encrypt = function(reformedArray) {
+  //set blank string to hold each 5 letter word of our final encrypted message
+  let encryptedMessage = "";
+  //set a count to work through five letter chunks of the array.
+  let count = 0;
+  //work through five letter chunks until and then slice them form reformedArray, join them, add a space after, and push to blank string.
+  for (let i = 0; i < reformedArray.length; i++) {
+    if (i % 5 === 0) {
+      count += 1;
+      encryptedMessage += reformedArray.slice(i-5, i).join("") + " ";
+    }
+  }
+  //for left over words.
+  if ((count-1)*5 <= reformedArray.length) {
+    encryptedMessage += reformedArray.slice((count-1)*5).join("");
+  }
+  return encryptedMessage;
+}
 
 
 //User logic
@@ -68,7 +91,8 @@ $(document).ready(function() {
       let size = squareSize(cleanString.length);
       let grid = makeGrid(cleanString, size);
       let reformed = reform(grid);
-      console.log(reformed);
+      let encrypted = encrypt(reformed);
+      console.log(encrypted);
     }
   });
 });
